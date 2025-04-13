bbc5c54ff15a5f528241b73ce572679ef4b0a21c
Toni Vanhala <toni.vanhala@reaktor.com>
2025-04-13 16:33:15 +0300

    refactor(test): move db-level test to correct namespace

--- celery_decipher/decipher/test_db.py
+++ celery_decipher/decipher/test_db.py
@@ -1,4 +1,8 @@
-from celery_decipher.decipher.db import get_source_text, insert_source_text
+from celery_decipher.decipher.db import get_candidates, get_source_text, insert_source_text
+from celery_decipher.decipher.solver import (
+    POPULATION_SIZE,
+    initial_guess,
+)
 
 
 def test_insert(testdb_cursor):
@@ -7,3 +11,12 @@ def test_insert(testdb_cursor):
     assert source_text_id is not None
     retrieved_text = get_source_text(testdb_cursor, source_text_id)
     assert retrieved_text == text
+
+
+def test_initial_guess(testdb_cursor):
+    text = "Smoky smoke test"
+    source_text_id = insert_source_text(testdb_cursor, text)
+    initial_guess(testdb_cursor, source_text_id)
+    candidates = get_candidates(testdb_cursor, source_text_id)
+    assert candidates is not None
+    assert len(candidates) == POPULATION_SIZE
